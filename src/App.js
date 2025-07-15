401e4094b886739fcd935ae5b3ee478a29d12e45
Akki07-code <1akshaymohpal@gmail.com>
2025-07-15 23:29:28 +0545

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RunnerGame from './RunnerGame';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Runner Game!</h1>
+      <RunnerGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RunnerGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const RunnerGame = () => {
+  return (
+    <div className="runner-game">
+      <h2>Runner Game</h2>
+      <p>Game will go here!</p>
+    </div>
+  );
+};
+
+export default RunnerGame; 
